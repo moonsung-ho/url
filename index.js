57d9f2a964a7a9d4bb0329e967b67a077cf92ea8
moonsung-ho <sungho.moon@aol.com>
2023-09-30 19:52:57 +0900

    접속수 로깅 시스템 구현

--- index.js
+++ index.js
@@ -9,13 +9,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 mongoose
   .connect(
-    `mongodb+srv://moonsungho:${process.env.MONGODB_PW}@urlshortener.ju9kyl1.mongodb.net/?retryWrites=true&w=majority`,
-    {
-      // useNewUrlPaser: true,
-      // useUnifiedTofology: true,
-      // useCreateIndex: true,
-      // useFindAndModify: false,
-    }
+    `mongodb+srv://moonsungho:${process.env.MONGODB_PW}@urlshortener.ju9kyl1.mongodb.net/?retryWrites=true&w=majority`
   )
   .then(() => console.log("MongoDB conected"))
   .catch((err) => {
@@ -26,6 +20,7 @@ mongoose
 const urlSchema = new mongoose.Schema({
   url: String,
   to: String,
+  clicked: Number,
 });
 const model = mongoose.model("url", urlSchema);
 
@@ -33,7 +28,7 @@ app.get("/shorten", (req, res) => {
   model.find({ to: req.query.to }).then((result) => {
     console.log(result);
     if (result.length === 0) {
-      model.create({ url: req.query.url, to: req.query.to });
+      model.create({ url: req.query.url, to: req.query.to, clicked: 0 });
       res.json({ status: "successed" });
     } else {
       res.json({ status: "failed" });
@@ -45,11 +40,37 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/frontend/");
 });
 
+app.get("/*/stats", (req, res) => {
+  model
+    .findOne({ to: req.params[0] })
+    .then((result) => {
+      res.send(
+        `현재까지 ${result.url}로 향하는 단축 URL
+        "${result.to}"은(는) ${result.clicked.toString()}번 열렸습니다.`
+      );
+    })
+    .catch((error) => {
+      res.redirect("/?type=urldoesnotexist");
+      console.log(error);
+    });
+});
+
 app.get("/*", (req, res) => {
   console.log(req.params);
   model
     .findOne({ to: req.params[0] })
     .then((result) => {
+      model
+        .updateOne(
+          { to: req.params[0] },
+          { $set: { clicked: result.clicked + 1 } }
+        )
+        .then(() => {
+          console.log("clicked updated to", result.clicked + 1);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
       res.redirect(result.url);
     })
     .catch((error) => {
